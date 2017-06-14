8867774b9763897c7a08ecad2ff7430b3e16c14b
shufo <meikyowise@gmail.com>
2017-06-15 01:11:15 +0900

    Add metadata

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule Fcmex.Mixfile do
     [app: :fcmex,
      version: "0.1.0",
      elixir: "~> 1.4",
+     description: description(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      test_coverage: [tool: ExCoveralls],
      preferred_cli_env: [
        "coveralls": :test,
@@ -26,6 +28,12 @@ defmodule Fcmex.Mixfile do
     [extra_applications: [:logger, :retry]]
   end
 
+  defp description do
+    """
+    A Firebase Cloud Message client for Elixir
+    """
+  end
+
   defp deps do
     [
       {:httpoison, ">= 0.0.0"},
@@ -36,6 +44,18 @@ defmodule Fcmex.Mixfile do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:exvcr, ">= 0.0.0", only: [:test, :dev]},
       {:excoveralls, ">= 0.0.0", only: :test},
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Shuhei Hayashibara"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/shufo/fcmex",
+        "Docs" => "https://hexdocs.pm/fcmex"
+      }
     ]
   end
 end
