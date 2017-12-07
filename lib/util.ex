b3985d827e38915016f20536201e03b98f3f8e9b
shufo <meikyowise@gmail.com>
2017-12-07 18:02:29 +0900

    Format util.ex

--- lib/util.ex
+++ lib/util.ex
@@ -9,15 +9,23 @@ defmodule Fcmex.Util do
   @client_error_status 400..499
   @server_error_status 500..599
 
-  def parse_result({:ok, %Response{status_code: status, body: body}}) when status in @success_status, do: {:ok, Poison.decode!(body)}
-  def parse_result({:ok, %Response{status_code: status, body: body}}) when status in @client_error_status do
+  def parse_result({:ok, %Response{status_code: status, body: body}})
+      when status in @success_status,
+      do: {:ok, Poison.decode!(body)}
+
+  def parse_result({:ok, %Response{status_code: status, body: body}})
+      when status in @client_error_status do
     body
-    |> Poison.decode
+    |> Poison.decode()
     |> case do
-      {:ok, decoded} -> {:error, decoded}
-      {:error, _}    -> {:error, body}
-    end
+         {:ok, decoded} -> {:error, decoded}
+         {:error, _} -> {:error, body}
+       end
   end
-  def parse_result({:ok, %Response{status_code: status, body: body}}) when status in @server_error_status, do: body
+
+  def parse_result({:ok, %Response{status_code: status, body: body}})
+      when status in @server_error_status,
+      do: body
+
   def parse_result({_, %Response{status_code: _, body: _} = response}), do: response
 end
