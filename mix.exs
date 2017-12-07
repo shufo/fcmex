cae141761bceb6297b773366d7a4671c5361b916
shufo <meikyowise@gmail.com>
2017-12-07 18:02:36 +0900

    Format mix.exs

--- mix.exs
+++ mix.exs
@@ -2,25 +2,27 @@ defmodule Fcmex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :fcmex,
-     version: "0.1.1",
-     elixir: "~> 1.4",
-     description: description(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [
-       "coveralls": :test,
-       "coveralls.detail": :test,
-       "coveralls.post": :test,
-       "coveralls.html": :test,
-       vcr: :test,
-       "vcr.delete": :test,
-       "vcr.check": :test,
-       "vcr.show": :test
-     ],
-     deps: deps()]
+    [
+      app: :fcmex,
+      version: "0.1.1",
+      elixir: "~> 1.4",
+      description: description(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        vcr: :test,
+        "vcr.delete": :test,
+        "vcr.check": :test,
+        "vcr.show": :test
+      ],
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -43,7 +45,7 @@ defmodule Fcmex.Mixfile do
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:exvcr, ">= 0.0.0", only: [:test, :dev]},
-      {:excoveralls, ">= 0.0.0", only: :test},
+      {:excoveralls, ">= 0.0.0", only: :test}
     ]
   end
 
