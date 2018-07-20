cdbf2a1a023b88adc170307676004205bb15e23b
shufo <meikyowise@gmail.com>
2018-07-20 23:04:48 +0900

    Bump fcmex to 0.2.1 #8, #9, #10

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Add to dependencies
 
 ```elixir
 def deps do
-  [{:fcmex, "~> 0.2.0"}]
+  [{:fcmex, "~> 0.2.1"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fcmex.Mixfile do
   def project do
     [
       app: :fcmex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.4",
       description: description(),
       build_embedded: Mix.env() == :prod,
