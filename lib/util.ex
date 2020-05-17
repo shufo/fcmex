b306d89778dd98fc16bdc08bf43b1a5d36a748a9
shufo <meikyowise@gmail.com>
2020-05-18 04:52:09 +0900

    fix: 🐛 handle HTTPoison error response
    ✅ Closes: #30

--- lib/util.ex
+++ lib/util.ex
@@ -4,11 +4,23 @@ defmodule Fcmex.Util do
   "
 
   alias HTTPoison.Response
+  alias HTTPoison.Error
 
   @success_status 200..299
   @client_error_status 400..499
   @server_error_status 500..599
 
+  @doc ~S"""
+  Parses the given FCM response.
+
+  ## Examples
+
+      iex> Fcmex.Util.parse_result({:ok, %HTTPoison.Response{status_code: 200, body: "{\"a\": 1}"}})
+      {:ok, Poison.decode!("{\"a\": 1}")}
+
+      iex> Fcmex.Util.parse_result({:error, %HTTPoison.Error{id: 1, reason: "something goes wrong"}})
+      {:error, %HTTPoison.Error{id: 1, reason: "something goes wrong"}}
+  """
   def parse_result({:ok, %Response{status_code: status, body: body}})
       when status in @success_status,
       do: {:ok, Poison.decode!(body)}
@@ -27,5 +39,7 @@ defmodule Fcmex.Util do
       when status in @server_error_status,
       do: body
 
+  def parse_result({:error, %Error{id: _, reason: _} = response}), do: {:error, response}
+  def parse_result({:error, response}), do: {:error, response}
   def parse_result({_, %Response{status_code: _, body: _} = response}), do: response
 end
